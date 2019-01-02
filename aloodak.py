3056963ff3155e86666657b819ed1bd648b21735
frowzy <frowzy_penguin@protonmail.com>
2019-01-02 20:56:26 +0000

    Replace aloodak.py

--- aloodak.py
+++ aloodak.py
@@ -13,7 +13,16 @@ last24hour_rate = str(soup('span', {'id' : 'ContentPlaceHolder1_lblAqi24h'})[0])
 last24hour_status = str(soup('span', {'id' : 'ContentPlaceHolder1_lblAqi24hDesc'})[0]).split('>')[1].split("<")[0]
 rightnow_rate = str(soup('span', {'id' : 'ContentPlaceHolder1_lblAqi3h'})[0]).split('>')[1].split("<")[0]
 rightnow_status = str(soup('span', {'id' : 'ContentPlaceHolder1_lblAqi3hDesc'})[0]).split('>')[1].split("<")[0]
-temprature = str(soup('span', {'id' : 'ContentPlaceHolder1_lblCurrentTemp'})[0]).split(">")[3].split("<")[0][1]
-windspeed =  str(soup('span', {'id' : 'ContentPlaceHolder1_lblCurrentWind'})[0]).split('>')[1].split("<")[0]
+try:
+    temprature = str(soup('span', {'id' : 'ContentPlaceHolder1_lblCurrentTemp'})[0]).split(">")[3].split("<")[0][1]
+except: 
+    temprature = "?"
+    pass
+try :
+    windspeed =  int(str(soup('span', {'id' : 'ContentPlaceHolder1_lblCurrentWind'})[0]).split('>')[1].split("<")[0])
+except :
+    windspeed = "?"
+    pass
+
 print(rtl("وضعیت آب و هوا "))
 print(rtl("\nشاخص آلودگی هوا \n"),rtl("\nهمین الآن \n"),rightnow_rate,rtl(rightnow_status),rtl("\n\nروز پیش \n"),last24hour_rate,rtl(last24hour_status),rtl("\n\nدما \n"),temprature,"℃\n",rtl("\nسرعت یاد \n"),windspeed,"m/s")
