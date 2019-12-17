5eb7440a0f96df9a7456601c66bc9f2ce1a6c32a
frowzy <frowzy_penguin@protonmail.com>
2019-12-17 18:58:03 +0330

    bug fixed

--- aloodak.py
+++ aloodak.py
@@ -1,31 +1,57 @@
 import requests
-from bs4 import BeautifulSoup
-from rtl import rtl
-from khayyam import *
-print("\n", rtl(JalaliDate.today().strftime('%A %d %B %Y')), "\n")
-weather_url = "https://airnow.tehran.ir"
-weather_req = requests.get(weather_url)
-if (weather_req.ok != True):
-    quit()
-weather_src = weather_req.text
-soup = BeautifulSoup(weather_src, "html.parser")
-last24hour_rate = soup.find(id="ContentPlaceHolder1_lblAqi24h").text
-last24hour_img = soup.find(id="ContentPlaceHolder1_imgAqi24")['src']
-last24hour_status = soup.find(id="ContentPlaceHolder1_lblAqi24hDesc").text
-rightnow_rate = soup.find(id="ContentPlaceHolder1_lblAqi3h").text
-rightnow_rate_img = soup.find(id="ContentPlaceHolder1_imgAqi3")['src']
-rightnow_status = soup.find(id="ContentPlaceHolder1_lblAqi3hDesc").text
-try:
-    temprature = soup.find(
-        id="ContentPlaceHolder1_lblCurrentTemp").text.split(" ")[2]
-except:
-    temprature = "?"
-try:
-    windspeed = soup.find(id="ContentPlaceHolder1_lblCurrentWind").text
-except:
-    windspeed = "?"
-
-
-print(rtl("وضعیت آب و هوا "))
-print(rtl("\nشاخص آلودگی هوا \n"), rtl("\nهمین الآن \n"), rightnow_rate, rtl(rightnow_status), rtl("\n\nروز پیش \n"),
-      last24hour_rate, rtl(last24hour_status), rtl("\n\nدما \n"), temprature, "℃\n", rtl("\nسرعت یاد \n"), windspeed)
+import rtl
+from bs4 import BeautifulSoup as bs
+from PIL import Image, ImageDraw, ImageFont
+# defining textcolor / light/dark colors has calculated by hps
+bg={\
+ 'bg-aqua': (0, 0, 0),\
+ 'bg-blue': (255, 255, 255),\
+ 'bg-green': (0, 0, 0),\
+ 'bg-light-blue': (0, 0, 0),\
+ 'bg-lime': (0, 0, 0),\
+ 'bg-navy': (255, 255, 255),\
+ 'bg-olive': (0, 0, 0),\
+ 'bg-red': (0, 0, 0),\
+ 'bg-teal': (0, 0, 0),\
+ 'bg-yellow': (0, 0, 0),\
+ 'bg-yellowgreen': (255, 255, 255)}
+# for replacing english digits with persian ones
+digits = {'1':'۱', '2':'۲', '3':'۳', '4':'۴', '5':'۵', '6':'۶', '7':'۷', '8':'۸', '9':'۹', '0':'۰'}
+def en2per(string):
+    # it parses string in list for acessing to string charachters
+    chars = list(map(lambda x: digits[x] if x.isdecimal() else x,list(string)))
+    # it returns an string of above list elements wich joined
+    return ''.join([str(x) for x in chars]) 
+class aloodak:
+    def __init__(self):
+        pass
+    def parser():
+        # request webserver and parsing data
+        soup = bs(requests.get("http://airnow.tehran.ir/home/home.aspx").text, "html.parser")
+        image_stat = soup.find_all(attrs={'id':'ContentPlaceHolder1_lblAqi3hBoxInfo23'})[0].img['src'].split("/")[-1]
+        pollution_rate = int(soup.find_all(attrs={'id':'ContentPlaceHolder1_lblAqi3h'})[0].text)
+        color = soup.find_all(attrs={'id':'ContentPlaceHolder1_lblAqi3hBoxInfo23'})[0]['class'][1]
+        status = rtl.rtl(soup.find_all(attrs={'id':'ContentPlaceHolder1_lblAqi3hDesc'})[0].text.split()[0])
+        return {'color' : color,'polution_rate': pollution_rate, 'image_stat': image_stat, 'status' : rtl.rtl(status)}
+class image_maker():
+    def __init__(self,data):
+        self.name = "report.png"
+        self.number_font = ImageFont.truetype("Sahel-Black.ttf", 140) # rate font style
+        self.name_font =  ImageFont.truetype("Sahel-Black.ttf", 20) # title font style
+        self.badge_font = ImageFont.truetype("Sahel-Bold.ttf", 20) # badge font style
+        self.status_font =  ImageFont.truetype("Sahel-Black.ttf", 50) # status font style
+        self.color = data['color'] # image color -> for example 'bg-green'
+        self.rate = str(data['polution_rate']) # pollution rate
+        self.status = data['status'] # pillution status
+        self.status = "ناسالم" if self.status.count("حساس") else self.status # changing long status
+    def draw(self):
+        self.image = Image.open(f"{self.color}.png") # opening image
+        self.draw_object = ImageDraw.Draw(self.image) # loafing draw module 
+        # wrting data over image
+        self.draw_object.text((100, 30), rtl.rtl("شاخص آلودگی هوا"), bg[self.color], font=self.name_font) 
+        self.draw_object.text((50, 50) if len(str(self.rate)) == 3 else (100,50), en2per(self.rate),bg[self.color], font=self.number_font)
+        self.draw_object.text((400, 100), rtl.rtl(self.status),bg[self.color], font=self.status_font)
+        self.draw_object.text((175,250), rtl.rtl("اطلاع‌رسانی غیر‌رسمی آلودک"),bg[self.color],font=self.badge_font) 
+        # saving image
+        self.image.save(self.name)
+        return None
---
+++ app.py
@@ -0,0 +1,4 @@
+from aloodak import * 
+data = aloodak.parser()
+image = image_maker(data)
+image.draw()
