8093305d94056a003f2dec7b9f5047e1e0f34fcb
frowzy <frowzy_penguin@protonmail.com>
2019-12-22 13:05:23 +0330

    aoo is usable

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from aloodak import * 
-data = aloodak.parser()
-info = info_maker(data)
-info.draw()
-info.cpation()
+from aloodak import *
+import os
+import time
+status = ""
+while True:
+    data = aloodak.parser()
+    info = info_maker(data)
+    info.draw()
+    info.cpation()
+    if status != os.popen("sha1sum report.png").read().split[0]: 
+        os.system("python3 bot.py")
+    else:
+        time.sleep(300)
